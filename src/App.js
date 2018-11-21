b7291ea084d482ec00c430a6608d3388db2cab07
Brian Dombrowski <bdombro@gmail.com>
2018-11-20 19:02:00 -0500

    All Events uses hooks now

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, {Suspense} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import "semantic-ui-css/semantic.min.css";
 import 'react-datepicker/dist/react-datepicker.css';
 
 import appSyncConfig from "./aws-exports";
-import { ApolloProvider } from "react-apollo";
-import AWSAppSyncClient, { defaultDataIdFromObject } from "aws-appsync";
-import { Rehydrated } from "aws-appsync-react";
+import {ApolloProvider} from "react-apollo";
+import {ApolloProvider as ApolloHooksProvider} from 'react-apollo-hooks';
+import AWSAppSyncClient, {defaultDataIdFromObject} from "aws-appsync";
+import {Rehydrated} from "aws-appsync-react";
 
 import './App.css';
 import AllEvents from './Components/AllEvents';
@@ -14,53 +15,55 @@ import NewEvent from './Components/NewEvent';
 import ViewEvent from './Components/ViewEvent';
 
 const Home = () => (
-  <div className="ui container">
-    <AllEvents />
-  </div>
+    <div className="ui container">
+        <AllEvents/>
+    </div>
 );
 
 const App = () => (
-  <Router>
-    <div>
-      <Route exact={true} path="/" component={Home} />
-      <Route path="/event/:id" component={ViewEvent} />
-      <Route path="/newEvent" component={NewEvent} />
-    </div>
-  </Router>
+    <Router>
+        <div>
+            <Route exact={true} path="/" component={Home}/>
+            <Route path="/event/:id" component={ViewEvent}/>
+            <Route path="/newEvent" component={NewEvent}/>
+        </div>
+    </Router>
 );
 
 const client = new AWSAppSyncClient({
-  url: appSyncConfig.aws_appsync_graphqlEndpoint,
-  region: appSyncConfig.aws_appsync_region,
-  auth: {
-    type: appSyncConfig.aws_appsync_authenticationType,
-    apiKey: appSyncConfig.aws_appsync_apiKey,
-  },
-  cacheOptions: {
-    dataIdFromObject: (obj) => {
-      let id = defaultDataIdFromObject(obj);
+    url: appSyncConfig.aws_appsync_graphqlEndpoint,
+    region: appSyncConfig.aws_appsync_region,
+    auth: {
+        type: appSyncConfig.aws_appsync_authenticationType,
+        apiKey: appSyncConfig.aws_appsync_apiKey,
+    },
+    cacheOptions: {
+        dataIdFromObject: (obj) => {
+            let id = defaultDataIdFromObject(obj);
 
-      if (!id) {
-        const { __typename: typename } = obj;
-        switch (typename) {
-          case 'Comment':
-            return `${typename}:${obj.commentId}`;
-          default:
+            if (!id) {
+                const {__typename: typename} = obj;
+                switch (typename) {
+                    case 'Comment':
+                        return `${typename}:${obj.commentId}`;
+                    default:
+                        return id;
+                }
+            }
+
             return id;
         }
-      }
-
-      return id;
     }
-  }
 });
 
 const WithProvider = () => (
-  <ApolloProvider client={client}>
-    <Rehydrated>
-      <App />
-    </Rehydrated>
-  </ApolloProvider>
+    <ApolloProvider client={client}>
+        <ApolloHooksProvider client={client}>
+            <Rehydrated>
+                <App/>
+            </Rehydrated>
+        </ApolloHooksProvider>
+    </ApolloProvider>
 );
 
 export default WithProvider;
--- src/Components/AllEvents.js
+++ src/Components/AllEvents.js
@@ -1,48 +1,41 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, {Fragment} from "react";
+import {Link} from "react-router-dom";
+import {useQuery, useMutation} from 'react-apollo-hooks';
+import gql from "graphql-tag";
 
-import { graphql, compose, withApollo } from "react-apollo";
 import QueryAllEvents from "../GraphQL/QueryAllEvents";
 import MutationDeleteEvent from "../GraphQL/MutationDeleteEvent";
 
 import moment from "moment";
 
-class AllEvents extends Component {
-
-    state = {
-        busy: false,
-    }
-
-    static defaultProps = {
-        events: [],
-        deleteEvent: () => null,
-    }
-
-    async handleDeleteClick(event, e) {
+const RenderEvent = ({event}) => {
+    const deleteEvent = useMutation(gql(`
+        mutation($id: ID!) {
+          deleteEvent(id: $id) {
+            id
+            name
+            where
+            when
+            description
+            comments {
+              items {
+                commentId
+              }
+            }
+          }
+        }`),
+        {
+            variables: { id: event.id },
+        }
+    );
+    const handleDeleteClick = async (e) => {
         e.preventDefault();
-
         if (window.confirm(`Are you sure you want to delete event ${event.id}`)) {
-            const { deleteEvent } = this.props;
-
             await deleteEvent(event);
         }
-    }
+    };
 
-    handleSync = async () => {
-        const { client } = this.props;
-        const query = QueryAllEvents;
-
-        this.setState({ busy: true });
-
-        await client.query({
-            query,
-            fetchPolicy: 'network-only',
-        });
-
-        this.setState({ busy: false });
-    }
-
-    renderEvent = (event) => (
+    return (
         <Link to={`/event/${event.id}`} className="card" key={event.id}>
             <div className="content">
                 <div className="header">{event.name}</div>
@@ -58,78 +51,70 @@ class AllEvents extends Component {
             <div className="extra content">
                 <i className="icon comment"></i> {event.comments.items.length} comments
             </div>
-            <button className="ui bottom attached button" onClick={this.handleDeleteClick.bind(this, event)}>
+            <button className="ui bottom attached button" onClick={handleDeleteClick.bind(this)}>
                 <i className="trash icon"></i>
                 Delete
             </button>
         </Link>
+    )
+};
+
+export const AllEvents = () => {
+    const busy = false;
+    const {data, error} = useQuery(gql(`
+        query {
+          listEvents(limit: 1000) {
+            items {
+              id
+              name
+              where
+              when
+              description
+              comments {
+                items {
+                  commentId
+                }
+              }
+            }
+          }
+        }`));
+    if (error) return `Error! ${error.message}`;
+
+    const handleSync = () => null;
+
+    const handleDeleteClick = async (event, e) => {
+        e.preventDefault();
+
+        if (window.confirm(`Are you sure you want to delete event ${event.id}`)) {
+            const {deleteEvent} = this.props;
+
+            await deleteEvent(event);
+        }
+    };
+
+
+
+    return (
+        <div>
+            <div className="ui clearing basic segment">
+                <h1 className="ui header left floated">All Events Hooks</h1>
+                <button className="ui icon left basic button" onClick={handleSync} disabled={busy}>
+                    <i aria-hidden="true" className={`refresh icon ${busy && "loading"}`}></i>
+                    Sync with Server
+                </button>
+            </div>
+            <div className="ui link cards">
+                <div className="card blue">
+                    <Link to="/newEvent" className="new-event content center aligned">
+                        <i className="icon add massive"></i>
+                        <p>Create new event</p>
+                    </Link>
+                </div>
+                {data.listEvents.items.map(event => <RenderEvent key={event.id} event={event}/>)}
+            </div>
+        </div>
     );
 
-    render() {
-        const { busy } = this.state;
-        const { events } = this.props;
 
-        return (
-            <div>
-                <div className="ui clearing basic segment">
-                    <h1 className="ui header left floated">All Events</h1>
-                    <button className="ui icon left basic button" onClick={this.handleSync} disabled={busy}>
-                        <i aria-hidden="true" className={`refresh icon ${busy && "loading"}`}></i>
-                        Sync with Server
-                    </button>
-                </div>
-                <div className="ui link cards">
-                    <div className="card blue">
-                        <Link to="/newEvent" className="new-event content center aligned">
-                            <i className="icon add massive"></i>
-                            <p>Create new event</p>
-                        </Link>
-                    </div>
-                    {[].concat(events).sort((a, b) => a.when.localeCompare(b.when)).map(this.renderEvent)}
-                </div>
-            </div>
-        );
-    }
-
-}
-
-export default withApollo(compose(
-    graphql(
-        QueryAllEvents,
-        {
-            options: {
-                fetchPolicy: 'cache-first',
-            },
-            props: ({ data: { listEvents = { items: [] } } }) => ({
-                events: listEvents.items
-            })
-        }
-    ),
-    graphql(
-        MutationDeleteEvent,
-        {
-            options: {
-                update: (proxy, { data: { deleteEvent } }) => {
-                    const query = QueryAllEvents;
-                    const data = proxy.readQuery({ query });
-
-                    data.listEvents.items = data.listEvents.items.filter(event => event.id !== deleteEvent.id);
-
-                    proxy.writeQuery({ query, data });
-                }
-            },
-            props: (props) => ({
-                deleteEvent: (event) => {
-                    return props.mutate({
-                        variables: { id: event.id },
-                        optimisticResponse: () => ({
-                            deleteEvent: {
-                                ...event, __typename: 'Event', comments: { __typename: 'CommentConnection', items: [] }
-                            }
-                        }),
-                    });
-                }
-            })
-        }
-    )
-)(AllEvents));
+};
+export default AllEvents;
